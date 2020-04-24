5745f0fbfb2538a4606c67a9d02bd585d97aafe9
Miguel Maia <miguel@cupdev.com.br>
2020-04-24 00:44:57 -0300

    pagina de login

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logoImg from '../../assets/logo.svg';
+import { FiLogIn } from 'react-icons/fi';
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <form>
+          <h1>Faça seu logon</h1>
+          <input type="text" placeholder="E-email" />
+          <input type="password" placeholder="Senha" />
+          <button type="submit">Entrar</button>
+          <a href="forgot">Esqueci minha senha</a>
+        </form>
+        <a href="login">
+          <FiLogIn />
+          Criar Conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default SignIn;
