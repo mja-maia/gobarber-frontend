704bca9a0fe917f2e822df488c5b755657437592
Miguel Maia <miguel@cupdev.com.br>
2020-04-24 01:16:37 -0300

    separando em components input e button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logoImg from '../../assets/logo.svg';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
@@ -10,9 +12,14 @@ const SignIn: React.FC = () => {
         <img src={logoImg} alt="GoBarber" />
         <form>
           <h1>Faça seu logon</h1>
-          <input type="text" placeholder="E-email" />
-          <input type="password" placeholder="Senha" />
-          <button type="submit">Entrar</button>
+          <Input name="email" icon={FiMail} type="text" placeholder="E-email" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Entrar</Button>
           <a href="forgot">Esqueci minha senha</a>
         </form>
         <a href="login">
