8d2d8ea21a61c5dc14e29fd21f73636ccf793149
Miguel Maia <miguel@cupdev.com.br>
2020-04-24 14:19:38 -0300

    finalizado tela de signup e signin com validações

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,75 @@
+import styled, { css } from 'styled-components';
+
+import Tooltip from '../Tooltip';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  padding: 16px;
+  width: 100%;
+  background: #232129;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+
+  border: 2px solid #232129;
+  color: #666360;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9900;
+      border-color: #ff9900;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9900;
+    `}
+
+  input {
+    color: #f4ede8;
+    background: transparent;
+    flex: 1;
+    border: 0;
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0;
+  }
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+`;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import logoImg from '../../assets/logo.svg';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core'
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
+import getValidationErrors from '../../utils/getValidationErrors'
+import * as Yup from 'yup';
+
+import { Form } from '@unform/web';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object): Promise<void> => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
   return (
     <Container>
       <Background />
 
       <Content>
         <img src={logoImg} alt="GoBarber" />
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
-          <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
-          <Input name="email" icon={FiMail} type="text" placeholder="E-email" />
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-email" />
           <Input
             name="password"
             icon={FiLock}
@@ -23,7 +49,7 @@ const SignUp: React.FC = () => {
             placeholder="Senha"
           />
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
         <a href="login">
           <FiArrowLeft />
           Voltar para login
